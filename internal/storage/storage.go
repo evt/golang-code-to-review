bb7eea5c1c73171ac4e7f3ef7fbca4d9c308c84c
Eugene Toropov <eugene.toropov@gmail.com>
2021-12-03 09:13:53 +0300

    improved storage :art:

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,27 +1,58 @@
 package storage
 
-// Store is the memory key value store
+import (
+	"sync"
+)
+
+// Store is the thread safe in memory key value store
 type Store struct {
-	values map[string]string
+	sync.RWMutex
+	values map[string]interface{}
 }
 
+// NewStore creates an empty Store
 func NewStore() *Store {
-	return &Store{}
-}
-
-func (s *Store) Load(key string) string {
-	return s.values[key]
-}
-
-func (s *Store) Remove(key string) {
-	s.values[key] = ""
-}
-
-func (s *Store) Exist(key string) bool {
-	if s.values[key] == "" {
-		return true
+	return &Store{
+		values: make(map[string]interface{}),
 	}
-	return false
 }
 
+// Load returns value for a specified key
+func (s *Store) Load(key string) (interface{}, bool) {
+	s.RLock()
+	defer s.RUnlock()
+	result, ok := s.values[key]
+	return result, ok
+}
 
+// Remove removes given key
+func (s *Store) Remove(key string) {
+	delete(s.values, key)
+}
+
+// Exists checks if given key exists
+func (s *Store) Exists(key string) bool {
+	s.RLock()
+	defer s.RUnlock()
+	_, ok := s.values[key]
+	return ok
+}
+
+// Save persists given key/vale combination
+func (s *Store) Save(key string, value interface{}) error {
+	s.Lock()
+	defer s.Unlock()
+	s.values[key] = value
+	return nil
+}
+
+// LoadAll returns all key/value pairs from the store
+func (s *Store) LoadAll() (map[string]interface{}, error) {
+	s.RLock()
+	defer s.RUnlock()
+	copyValues := make(map[string]interface{}, len(s.values))
+	for k, v := range s.values {
+		copyValues[k] = v
+	}
+	return copyValues, nil
+}
