3beb383332104b716fd51c61c9db8430c4ed07ea
Eugene Toropov <eugene.toropov@gmail.com>
2022-01-16 12:42:14 +0300

    added another code snippet to review :ambulance:

---
+++ internal/mysqlstorage/mysqlstorage.go
@@ -0,0 +1,37 @@
+package mysqlstorage
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type MyStoreStore struct {
+	context.Context
+	Dbh *sql.DB
+}
+
+func myStoreConstructor(config map[string]string) MyStoreStore {
+	db, _ := sql.Open("mysql", "database="+config["db"])
+	return MyStoreStore{nil, db}
+}
+
+func (s MyStoreStore) Insert(items []string) {
+	stmt, err := s.Dbh.Prepare("INSERT INTO items(id, title) VALUES(?, ?")
+	if err != nil {
+		panic(err)
+	}
+
+	for i, item := range items {
+		stmt.ExecContext(s.Context, i, item)
+	}
+}
+
+func (s *MyStoreStore) Update(id int, title string) {
+	stmt, err := s.Dbh.Prepare(fmt.Sprintf("UPDATE INTO users SET title = '%s' WHERE id = %d", title, id))
+	if err != nil {
+		panic(err)
+	}
+
+	stmt.ExecContext(s.Context)
+}
